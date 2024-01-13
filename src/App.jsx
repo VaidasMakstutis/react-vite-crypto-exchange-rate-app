95b871510f095977d9b82c248f4831b4889c89a5
Vaidas <vaidas.makstutis@gmail.com>
2024-01-13 21:43:29 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <>
-      <h1 className="text-4xl text-center font-bold  text-green-700 pt-8">Welcome to Vite with TailwindCSS and React</h1>
+      <h1 className="text-4xl text-center font-bold  text-green-700 pt-8">Crypto Exchange Rate Web App</h1>
     </>
   );
 };
