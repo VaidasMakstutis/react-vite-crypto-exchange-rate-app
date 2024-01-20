98b26b25e7606926caf838183e341cd85c720523
Vaidas <vaidas.makstutis@gmail.com>
2024-01-20 23:08:13 +0200

    ExchangeRateUI props & props validation

--- src/components/UI/ExchangeRateUI.jsx
+++ src/components/UI/ExchangeRateUI.jsx
@@ -1,11 +1,30 @@
 import { Typography, Card } from "antd";
+import propTypes from "prop-types";
 
-export const ExchangeRateUI = () => {
+export const ExchangeRateUI = props => {
+  const { price, dataObj, currencySymbol } = props;
+  const toCurrency = dataObj.toCurrency;
+  let value = Number(price);
+  let currencyCode = toCurrency;
+
+  let currency = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: currencyCode
+  });
+
+  let formattedCurrency = currency.format(value);
+
   return (
     <div className="exchange-rate-ui">
-      <Card extra={3} bordered={false} style={{ width: 350, backgroundColor: "#4d4add", color: "#fff" }} size="default">
-        <Typography.Paragraph style={{ color: "#fff" }}>Bitcoin</Typography.Paragraph>
+      <Card extra={currencySymbol} bordered={false} style={{ width: 350, backgroundColor: "#4d4add", color: "#fff" }} size="default">
+        <Typography.Paragraph style={{ color: "#fff" }}>{formattedCurrency}</Typography.Paragraph>
       </Card>
     </div>
   );
+};
+
+ExchangeRateUI.propTypes = {
+  price: propTypes.string.isRequired,
+  dataObj: propTypes.object.isRequired,
+  currencySymbol: propTypes.string.isRequired
 };
